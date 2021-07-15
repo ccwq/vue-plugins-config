786eb762b4f8ed80cec16f63a86429c0d1e2b0f5
ccwq <scwqd@qq.com>
2021-07-15 15:29:39 +0800

    定位功能异步使用定位插件

--- src/plugins/amap/index.js
+++ src/plugins/amap/index.js
@@ -11,7 +11,7 @@ const defaultOptions = {
 
     // 需要使用的的插件列表，如比例尺'AMap.Scale'等
     "plugins": [
-        'AMap.Geolocation',
+        //'AMap.Geolocation',
     ],
 };
 
@@ -49,20 +49,26 @@ export const getLocation = function (options = {}) {
     return getAMap().then(AMap => {
 
         options = Object.assign({}, {
+
             // 是否使用高精度定位，默认：true
             enableHighAccuracy: true,
+
             // 设置定位超时时间，默认：无穷大
             timeout: 10000,
+
             // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
             buttonOffset: new AMap.Pixel(10, 20),
+
             //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
             zoomToAccuracy: true,
+
             //  定位按钮的排放位置,  RB表示右下
             buttonPosition: 'RB'
         }, options);
 
         return new Promise((resolve, reject) => {
 
+            //异步使用插件
             AMap.plugin('AMap.Geolocation', function () {
                 var geolocation = new AMap.Geolocation(options)
 
